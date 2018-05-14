5417dbc73aefa04cb699d35de62edc821407df78
JKimani77 <jkkimani77@gmail.com>
2018-05-14 08:34:20 +0300

    adds variables and functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+var rolldice = function() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
+function player(throwdice, actingscore, totalscore) {
+  this.diceroll = throwdice
+  this.actingscore = actingscore;
+  this.totalscore = totalscore;
+}
