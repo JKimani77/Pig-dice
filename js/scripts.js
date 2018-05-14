07481de1ef3596967302ad7fdf4632f2b55ae7d9
JKimani77 <jkkimani77@gmail.com>
2018-05-14 08:35:45 +0300

    adds prototype and new variables with alert functions

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,18 @@ function player(throwdice, actingscore, totalscore) {
   this.actingscore = actingscore;
   this.totalscore = totalscore;
 }
+var firstplayer = new player(0, 0, 0);
+var secondplayer = new player(0, 0, 0);
+player.prototype.firstroll = function() {
+  if (this.diceroll === 1) {
+    this.actingscore = 0;
+    alert(" Player Scored a 1, next player !")
+  } else {
+    this.actingscore += this.diceroll;
+  }
+}
+player.prototype.hold = function() {
+  this.totalscore += this.actingscore;
+  this.actingscore = 0;
+  alert(" Player has Held the game, next player turn ! ");
+}
